33e27c7090a4ee26a18e89538faa330de7a42c09
lkighy <1003027913@qq.com>
2020-04-05 16:08:34 +0800

    测试全局变量的使用方式

--- src/conf/conf.rs
+++ src/conf/conf.rs
@@ -15,43 +15,21 @@ pub struct Conf {
     session_name: String, // session 键名称
 }
 
-
-lazy_static! {
-    static ref conf: Conf = {
-       let f = File::open("config.json").expect("读取配置失败").read_to_string();
-       serde_json::from_str(f).expect("配置解析失败")
-    };
-    // static ref REDIS_CONNECTION = {
-    //     // lianjie redis
-    // };
-}
-
-fn init() {
-    let conf = Conf {
-        redis_connection: String::from(""),
-        mongodb_hostname: String::from(""),
-        mongodb_port: 0,
-        app_hostname: String::from(""),
-        app_port: String::from(""),
-        session_name: String::from(""),
-    };
-    let conf: Conf = {
-        let mut f = File::open("config.json").expect("读取配置失败");
-        let confStr;
-        f.read_to_string(&confStr);
-        serde_json::from_str(&confStr).expect("配置解析失败")
-    };
-
-}
-
-// pub fn init() -> redis::RedisResult<isize>{
-//     // connect to redis
-//     let client = redis::Client::open("redis://127.0.0.1/6379")?;
-//     let mut con = client.get_connection()?;
-//     // throw away the result, just make sure it does not fail
-//     let _ : () = con.set("my_key", 42)?;
-//     // read back the key and return it.  Because the return value
-//     // from the function is a result for integer this will automatically
-//     // convert into one.
-//     con.get("my_key")
+// #[derive(Deserialize, Debug)]
+// pub struct Conf {
+//     redis_connection: &'static str, // redis://127.0.0.1:6379
+//     mongodb_hostname: &'static str, // 127.0.0.1
+//     mongodb_port: i32, // mongodb 27017
+//     app_hostname: &'static str, // app 主键 127.0.0.1
+//     app_port: &'static str, // app 绑定端口
+//     session_name: &'static str, // session 键名称
 // }
+
+pub const conf:Conf = {
+    let mut f = File::open("config.json").expect("读取配置文件失败");
+    let mut confStr = String::new();
+    f.read_to_string(&mut confStr).expect("转换为字符串失败");
+    serde_json::from_str(&confStr).expect("配置解析失败")
+};
+
+
