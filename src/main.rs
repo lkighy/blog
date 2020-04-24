ab0db1f0723b027862125fcac5edb1f0e2ff557e
lkighy <1003027913@qq.com>
2020-04-24 16:08:16 +0800

    .env 代替 config.json 配置

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,53 @@
-use actix_web::{App, HttpServer};
-mod conf;
-mod app;
+#[macro_use]
+extern crate dotenv_codegen;
 
-use actix_web::middleware;
+use actix_web::{App, HttpServer};
+use actix_web::middleware::Logger;
+
+use actix_redis::{RedisActor, RedisSession};
+
+use rand;
 
 use mongodb::{Client};
 use mongodb::options::{ClientOptions, StreamAddress};
-use actix_redis::{RedisActor};
+use rand::Rng;
+
+
+mod conf;
+mod app;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-
+    // 转换 byte
+    let app_addr = format!("{}:{}", dotenv!("APP_HOSTNAME"), dotenv!("APP_PORT"));
     HttpServer::new(|| {
+        let mongodb_port = format!("{}", dotenv!("MONGODB_PORT"));
+        let mongodb_hostname = format!("{}", dotenv!("MONGODB_HOSTNAME"));
         let mongo = {
             let options = ClientOptions::builder()
                 .hosts(vec![
                     StreamAddress {
-                        hostname: conf::CONF.mongodb_hostname.into(),
-                        port: Some(conf::CONF.mongodb_port),
+                        hostname: mongodb_hostname.clone(),
+                        port: Some(mongodb_port.parse().expect("mongodb_port 不是数值类型")),
                     }
                 ])
                 .build();
             let client = Client::with_options(options).expect("连接mongodb 失败");
             client.database(conf::CONF.mongodb_name)
         };
-        let redis_addr = RedisActor::start(conf::CONF.redis_addr);
+        let redis_addr = dotenv!("REDIS_ADDR");
+        let redis = RedisActor::start(redis_addr);
+
+        let private_key = rand::thread_rng().gen::<[u8; 32]>();
         App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .wrap(RedisSession::new(redis_addr, &private_key))
             .data(mongo)
-            .data(redis_addr)
-            .warp(middleware::Logger::default())
+            .data(redis)
             .configure(app::v1::index_config)
     })
-    .bind(
-        format!("{}:{}", conf::CONF.app_hostname,
-            conf::CONF.app_port
-        )
-    )?
+    .bind(app_addr)?
     .run()
     .await
 }
