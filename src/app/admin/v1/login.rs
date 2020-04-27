185f144eecb7f1fbaf39ba4cf37bc0a12989b7c1
lkighy <1003027913@qq.com>
2020-04-27 17:47:16 +0800

    替换 redis 库

--- src/app/admin/v1/login.rs
+++ src/app/admin/v1/login.rs
@@ -2,13 +2,40 @@
 // 1.
 // 发送验证码
 use actix_web::{post, get, web, HttpRequest};
-use actix_redis::{RedisActor};
-use actix::Addr;
+// use actix_redis::{RedisActor};
+// use actix::Addr;
+
+// use redis;
+// use redis::Commands;
+extern crate redis;
+use redis::Connection;
+use std::net::SocketAddr;
+use self::redis::Commands;
+use std::sync::Mutex;
 
 #[get("/send-ckm")]
-pub async fn send_ckm(req: HttpRequest, _redis: web::Data<Addr<RedisActor>>) -> String {
+pub async fn send_ckm(req: HttpRequest, redis_con: web::Data<Mutex<Connection>>) -> String {
     // 获取 ip 地址，
-    // 查询该 ip 是否已被拉黑
+    let addr = match req.peer_addr() {
+        Some(addr) => addr,
+        None => panic!(),
+    };
+    let ip = match addr {
+        SocketAddr::V4(ipv4) => format!("{:?}", ipv4.ip()),
+        SocketAddr::V6(ipv6) => format!("{:?}", ipv6.ip()),
+    };
+    // todo 1: 查询该 ip 是否已被拉黑
+
+    // let _: () = *redis_a.sadd("blacklist", &ip).unwrap();
+    // let _:() = redis_con.lock().unwrap().sadd("blacklist", &ip).unwrap();
+    let mut redis_a = redis_con.lock().unwrap();
+    let _:() = redis_a.sadd("blacklist", &ip).unwrap();
+    // let _:() = redis_con.sadd("blacklist", &ip).unwrap();
+
+    // let _:() = *redis_con.get_ref().sadd("blacklist", &ip).unwrap();
+
+    // redis.get_ref().
+
     // 查询该 ip 是否已经提交了三次错误验证码
     // 查询 redis 缓存中是否有 验证码
     // 有
@@ -19,11 +46,11 @@ pub async fn send_ckm(req: HttpRequest, _redis: web::Data<Addr<RedisActor>>) -> 
     // 发送邮件到邮箱中。等待输入
     // redis.send()
 
-    format!("{:?}", req.connection_info())
+    format!("{}", ip)
 }
 
 #[post("/verify-ckm")]
-pub async fn verify_ckm(_redis: web::Data<Addr<RedisActor>>) -> String {
+pub async fn verify_ckm() -> String {
     // 获取 ip
     // 查询该 ip 下的验证码是否相同
     String::from("verify-ckm")
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 #[macro_use]
 extern crate dotenv_codegen;
+extern crate redis;
 
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 use actix_web::middleware::Logger;
 
-use actix_redis::{RedisActor, RedisSession};
+use actix_redis::{RedisSession};
 
 use mongodb::{Client};
 use mongodb::options::{ClientOptions, StreamAddress};
+
+// use redis::Commands;
+use std::sync::Mutex;
 
 // mod conf;
 mod app;
@@ -16,6 +20,8 @@ mod app;
 async fn main() -> std::io::Result<()> {
     // 转换 byte
     let app_addr = format!("{}:{}", dotenv!("APP_HOSTNAME"), dotenv!("APP_PORT"));
+
+
     HttpServer::new(|| {
         let mongodb_port = format!("{}", dotenv!("MONGODB_PORT"));
         let mongodb_hostname = format!("{}", dotenv!("MONGODB_HOSTNAME"));
@@ -32,20 +38,27 @@ async fn main() -> std::io::Result<()> {
             let client = Client::with_options(options).expect("连接mongodb 失败");
             client.database(&mongodb_name)
         };
+
         let redis_addr = dotenv!("REDIS_ADDR");
         let key = format!("{}", dotenv!("SESSION_KEY"));
+
+        let redis = web::Data::new({
+            let client = redis::Client::open(format!("redis://{}", redis_addr)).expect("创建 redis 客户端失败");
+            Mutex::new(client.get_connection().expect("redis 连接失败"))
+        });
+
         let private_key = key.as_bytes();
-        let redis = RedisActor::start(redis_addr);
+        // let actix_redis = RedisActor::start(redis_addr);
         // 生成随机 key, 这里应该用一个固定的随机 key 代替
+
         App::new()
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
             .wrap(RedisSession::new(redis_addr, private_key))
             .data(mongo)
-            .data(redis)
+            .app_data(redis.clone())
             .configure(app::v1::index_config)
             .configure(app::admin::login_config)
-
     })
     .bind(app_addr)?
     .run()
